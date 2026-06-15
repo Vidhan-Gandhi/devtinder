453a24a8f182cb57cfa81f971d42277ca0c1e76e
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-15 15:44:07 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,33 +1,49 @@
-// const express=require('express');
-// const app=express();
-
-// app.use("/test",(req,res)=>{
-//     res.send("This is test respomse");
-// });
-
-// app.use("/login",(req,res)=>{
-//     res.send("This is login response");     
-// });
-
-// app.use((req,res)=>{
-//     res.send('Hello World');
-// });
-
-
-// app.listen(3000,()=>{
-//     console.log('Server is running on port 3000');
-// });
-
 const express=require('express');
 const app=express();
 
-app.use("/signup",(req,res)=>{
-    res.send("This is signup response");    
+
+//To fetch data from database
+app.get("/signup",(req,res)=>{
+    res.send({firstName:"John",lastName:"Doe",email:"vg" });    
 })
 
-app.use((req,res)=>{
-    res.send('Hello World');    
-})
-app.listen(7777, ()=>{
-    console.log('Server is running on port 7777');  
-})
+//To post data on database
+app.post("/signup",(req,res)=>{
+    res.send("This is signup response");    
+});
+
+//To delete data from database
+app.delete("/signup",(req,res)=>{
+    res.send("This is delete response");    
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
+
+// const express=require('express');
+// const app=express();
+
+// app.use("/signup",(req,res)=>{
+//     res.send("This is signup response");    
+// })
+
+// app.use((req,res)=>{
+//     res.send('Hello World');    
+// })
+// app.listen(7777, ()=>{
+//     console.log('Server is running on port 7777');  
+// })
+
+// const express=require('express');
+// const app=express();
+// app.use("/signup",(req,res)=>{
+//     res.send("This is signup response");    
+// })
+// app.use((req,res)=>{
+//     res.send('Hello World');
+// })
+
+// app.listen(1405,()=>{
+//     console.log('Server is running on port 1405');   
+// })
