41721d43b56494fa7d83b2d6d7d57b946471787d
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-15 18:56:08 +0530

    File 3 using HTTP methods to create routes for different types of requests

--- src/app.js
+++ src/app.js
@@ -13,7 +13,41 @@ app.listen(3000,()=>{
 app.use("/login",(req,res)=>{
     res.send("This is response from  login page");
 });
+// We can also create a route for the home page and it will be executed when the user accesses the home page url: http://localhost:3000/homepage
+app.use("/homepage",(req,res)=>{
+    res.send("This is response from home page");
+});
 //app.use can process any type of request (GET, POST, PUT, DELETE) and it will be executed for every request to the server
 app.use("/",(req,res)=>{
     res.send("This is response from server");
+});
+//step5:Using HTTP methods to create routes for different types of requests
+//GET request for the login page
+//GET request is used to retrieve data from the server and it will be executed when the user accesses the login page url: http://localhost:3000/login with a GET request and the data will be sent in the query string of the URL           
+app.get("/login",(req,res)=>{
+
+    res.send("{name:'VGBTE',age:22}");
+});
+//POST request for the login page
+//post request is used to send data to the server and it will be executed when the user submits a form or sends data to the server url: http://localhost:3000/login with a POST request and the data will be sent in the body of the request    
+app.post("/login",(req,res)=>{
+    res.send("This is response from POST request to login page");
+}); 
+//PUT request for the login page
+//PUT request is used to update data on the server and it will be executed when the user sends a PUT request to the login page url: http://localhost:3000/login with a PUT request and the data will be sent in the body of the request     
+app.put("/login",(req,res)=>{
+    res.send("This is response from PUT request to login page");
+}); 
+//DELETE request for the login page
+//DELETE request is used to delete data from the server and it will be executed when the user sends a DELETE request to the login page url: http://localhost:3000/login with a DELETE request and the data will be sent in the body of the request       
+app.delete("/login",(req,res)=>{
+    res.send("This is response from DELETE request to login page");
+}); 
+app.delete("/login",(req,res)=>{
+    res.send("This is response from DELETE request to login page");
+}); 
+//PATCH request for the login page
+//PATCH request is used to update a part of the data on the server and it will be executed when the user sends a PATCH request to the login page url: http://localhost:3000/login with a PATCH request and the data will be sent in the body of the request
+app.patch("/login",(req,res)=>{
+    res.send("This is response from PATCH request to login page");
 });
