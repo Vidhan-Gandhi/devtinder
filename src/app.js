eec8f7c892b3996405550614795c18e5a65b6461
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-15 18:42:37 +0530

    File 2 Creating Route and Request Handlers For server using app.use

--- src/app.js
+++ src/app.js
@@ -8,3 +8,12 @@ const app=express();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
+//step4: Creating a route and request handlers for the server
+// /login is the route and the callback function will be executed when the route is accessed url: http://localhost:3000/login
+app.use("/login",(req,res)=>{
+    res.send("This is response from  login page");
+});
+//app.use can process any type of request (GET, POST, PUT, DELETE) and it will be executed for every request to the server
+app.use("/",(req,res)=>{
+    res.send("This is response from server");
+});
