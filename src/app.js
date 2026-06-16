7167303f23a9f5df3ec39123e9c3cf59a4e59f11
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-16 23:17:43 +0530

    File 11 Authenticating Admin to enter Route

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
-//Middlewares
-
-//First 2 request handlers are middlewares and the last one is the final request handler
-//Middlewares are functions that do not send a response to the client, but they can modify the request and response objects, and they can also call the next middleware in the stack.
-app.get('/login',(req,res,next)=>{
-    console.log("Login Page");
-    next();
-},(req,res,next)=>{
-    console.log("Login Page Accessed");
-    next();
-},(req,res)=>{
-    res.send("Login Page");
+//Admin Authebtication
+//Admin will have a token to access the admin route if the token is correct then admin can access the route otherwise it will return unauthorized 401 error
+app.get('/admin',(req,res)=>{
+    const tokens="wxyz"; //This token will be used to authenticate the admin
+    const AuthenticateAdmin=tokens==="xyz"; //This will check if the token is correct or not this is function to authenticate the admin
+    if(AuthenticateAdmin){
+        res.send("Data send");
+    }else{
+        res.status(401).send("Unauthorized"); //This will return unauthorized error if the token is incorrect if i only write res.send("Unauthorized") then it will return 200 ok status code but we want to return 401 unauthorized error so we use res.status(401).send("Unauthorized")
+        // If 200 status code is returned then it will be considered as a successful request but in this case we want to return 401 unauthorized error so we use res.status(401).send("Unauthorized") to return 401 status code and message "Unauthorized"
+    }
+   ;
 });
