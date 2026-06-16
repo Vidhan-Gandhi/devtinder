28382e8f76ca22bd3139351ba94125db49a2e0d3
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-16 12:32:08 +0530

    File 10 Middlewares

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
-// Another method for creating multiple request handlers for the same route
-app.get('/hello',(req,res,next)=>{
-    console.log("Hello World!");
+//Middlewares
+
+//First 2 request handlers are middlewares and the last one is the final request handler
+//Middlewares are functions that do not send a response to the client, but they can modify the request and response objects, and they can also call the next middleware in the stack.
+app.get('/login',(req,res,next)=>{
+    console.log("Login Page");
     next();
+},(req,res,next)=>{
+    console.log("Login Page Accessed");
+    next();
+},(req,res)=>{
+    res.send("Login Page");
 });
-app.get('/hello',(req,res)=>{
-    res.send("Hello Again!");
-});
-//This is same as like we created two handlers for the same route. We can also use array of handlers for the same route.
