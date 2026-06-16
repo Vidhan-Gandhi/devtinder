d3837fee3aef99e2f46c2fe77906b3dbf9a74a9f
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-16 11:16:45 +0530

    File 8 Passing Request Handlers as Array

--- src/app.js
+++ src/app.js
@@ -4,18 +4,18 @@ app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
-//Step 1: Creating Multiple Requests Hadlers
-//If we not use next() method in first request handler then second request handler will not execute and we will get error "Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client" because we are trying to send response twice but we can only send response once for a request. So we need to use next() method to pass control to next request handler.
-//If both request handlers are sending response then the request handlers will execute in sequence and we will get response from  request handlers that eexecuted first but if we not use next() method in first request handler then second request handler will not execute and we will get error "Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client" because we are trying to send response twice but we can only send response once for a request. So we need to use next() method to pass control to next request handler.
-// So next() method is must when we have multiple request handlers for a route and we want to send response from both request handlers. If we not use next() method in first request handler then second request handler will not execute and we will get error "Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client" because we are trying to send response twice but we can only send response once for a request. So we need to use next() method to pass control to next request handler.
-app.get('/login',
-    (req,res,next)=>{
-    res.send("This is response from first request handler");
-    console.log("Response 1");
-    next();
-},
-(req,res)=>{
-    console.log("Response 2");
-    res.send("This is response from second request handler");
-    
-});
+// Array of Multiple Request Handlers
+
+//1. All request handlers are passed in an array there will be bo difference in the output of the code if we use array or not. The output will be same in both cases.
+app.get('/login', [(req, res, next) => {
+    console.log('First handler');
+    next(); // Pass control to the next handler
+}, (req, res) => {
+    console.log('Second handler');
+    res.send('Login successful');
+}, (req, res) => {
+    console.log('Third handler');
+    // This handler will not be reached because the response has already been sent
+}]);
+
+//Some of all request handlers can be passed in an array and some can be passed separately. The output will be same in both cases.
