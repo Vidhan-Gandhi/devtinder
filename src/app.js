8a22fddd7a7ba98b58211123e7454deae9aebc58
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-17 10:12:42 +0530

    File 13 ERROR HANDLING using error handling route

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
 const express=require('express');
 const app=express();
-const {adminauth}=require('./middlewares/AdminAuthentication'); //Importing the adminauth function from the AdminAuthentication.js file to use it as middleware in our routes.
-const {userauth}=require('./middlewares/UserAuthentication');
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
-//If multiple routes are authenticated, we can use middleware to authenticate them all at once.
 
-app.use('/admin',adminauth); //This line of code is using the adminauth middleware for all routes that start with /admin. This means that any request to a route that starts with /admin will first go through the adminauth middleware function to check if the user is an admin before allowing access to the route handler. If the user is not an admin, they will receive a 401 Unauthorized response. If the user is an admin, they will be able to access the route handler and receive the appropriate response.
-app.get('/admin/data',(req,res)=>{   //This line of code is defining a route handler for the GET request to the /admin/data route. If the user is an admin, they will receive a response with the message "Welcome to the admin panel". If the user is not an admin, they will receive a 401 Unauthorized response from the adminauth middleware function.
-    res.send("Welcome to the admin panel");
-   
+
+app.get('/admin',(req,res,next)=>{
+    console.log("Hello");
+    next(); // Simulating an error to demonstrate error handling
+    
+    
 });
-app.get('/admin/settings',(req,res)=>{ //This line of code is defining a route handler for the GET request to the /admin/settings route. If the user is an admin, they will receive a response with the message "Welcome to the admin settings". If the user is not an admin, they will receive a 401 Unauthorized response from the adminauth middleware function.
-    res.send("Welcome to the admin settings");  
+
+app.get('/admin',(req,res,next)=>{
+    throw new Error("Something went wrong!"); // Simulating an error to demonstrate error handling
 });
-app.use('/user',userauth); //This line of code is using the userauth middleware for all routes that start with /user. This means that any request to a route that starts with /user will first go through the userauth middleware function to check if the user is authenticated before allowing access to the route handler. If the user is not authenticated, they will receive a 401 Unauthorized response. If the user is authenticated, they will be able to access the route handler and receive the appropriate response.
-app.get('/user/data',(req,res)=>{ //This line of code is defining a route handler for the GET request to the /user/data route. If the user is authenticated, they will receive a response with the message "Welcome to the user panel". If the user is not authenticated, they will receive a 401 Unauthorized response from the userauth middleware function.
-    res.send("Welcome to the user panel");  
+
+app.use('/',(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Internal Server Error");
+    }
 });
-app.get('/user/settings',(req,res)=>{ //This line of code is defining a route handler for the GET request to the /user/settings route. If the user is authenticated, they will receive a response with the message "Welcome to the user settings". If the user is not authenticated, they will receive a 401 Unauthorized response from the userauth middleware function.
-    res.send("Welcome to the user settings");
-});
+
