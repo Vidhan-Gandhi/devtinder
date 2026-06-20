e0e30bb41f3efb25a97b09669417511d4f3acc19
Vidhan Gandhi <vgbtech2327@gmail.com>
2026-06-20 23:22:51 +0530

    File 15 Writing code in Database.js to connect database to project and require database.js file in app.js

--- src/app.js
+++ src/app.js
@@ -1,45 +1,7 @@
 const express=require('express');
 const app=express();
-
+require('./config/database'); // Importing the database configuration to establish a connection to MongoDB
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
-});
-
-//ERROR HANDLING
-
-// app.get('/admin',(req,res,next)=>{
-//     console.log("Hello");
-//     next(); // Simulating an error to demonstrate error handling
-    
-    
-// });
-
-// app.get('/admin',(req,res,next)=>{
-//     throw new Error("Something went wrong!"); // Simulating an error to demonstrate error handling
-//     // If error it skips the next middleware and goes to the error handling middleware
-// });
-
-// app.use('/',(err,req,res,next)=>{ // if we write this before error occurs, it will not work because error handling middleware should be defined after the route that may throw an error. This is because Express.js processes middleware in the order they are defined, so if the error handling middleware is defined before the route that may throw an error, it will not be able to catch the error.
-//     if(err){
-//         res.status(500).send("Internal Server Error");
-//     }
-// });
-
-app.get('/admin',(req,res,next)=>{
-    console.log("First Route"); 
-    next(); 
-    
-});
-app.get('/admin',(req,res,next)=>{
-    console.log("Second Route");
-    throw new Error("Something went wrong!");
-    next();
-});
-app.get('/admin',(req,res,next)=>{
-    console.log("Third Route");
-    res.send("hi");
-});
-app.use('/',(err,req,res,next)=>{
-    res.status(500).send("Internal Server Error");
 });
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,21 @@
-const mongoose=require('mongoose');
-mongoose.connect();
-const connectDB= async()=>{
-    await mongoose.connect(
-        "mongodb+srv://namastedev:vV0QOqSaDbM8FCWe@devtinder.sfstvbg.mongodb.net/?appName=DevTinder"
+const mongoose = require('mongoose'); // Importing the mongoose library
+// This function will handle the connection to the MongoDB database using async/await syntax.
+const connectDB = async () => {
+    try {
+        // 1. TRY to do the slow/risky thing
+        await mongoose.connect(
+            "mongodb+srv://namastedev:vV0QOqSaDbM8FCWe@devtinder.sfstvbg.mongodb.net/?appName=DevTinder"
+        );
+        
+        // 2. If the line above works, this line runs next!
+        console.log("Database connected successfully");
 
-
-    );
+    } catch (err) {
+        // 3. If the connect() fails, the code INSTANTLY jumps down here.
+        // It skips the success console.log entirely.
+        console.error("Database connection failed", err);
+    }
 };
 
-connectDB().then(()=>{
-    console.log("Database connected successfully");
-}).catch((err)=>{
-    console.error("Database connection failed",err);
-});
+// Now you just call the function! No .then() or .catch() needed down here.
+connectDB();
